e2d81cd840edbf138877223b40eb341526aceded
Dhanvith Shetty <79599525+DhanvithShetty@users.noreply.github.com>
2022-12-14 00:20:23 +0530

    Added a homepage for the database system

---
+++ home.php
@@ -0,0 +1,23 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inventory Management</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div>
+        <div class="text-container">
+            <h1 class="main-text">Inventory Management</h1>
+            <h2 class="sub-text">Click on the table that you want to view!</h2>
+        </div>
+        <div class="btn-wrapper">
+            <button class="btn-1" onclick="location.href='./Item/index.php'"><span class="btn-text">Item</span></button>
+            <button class="btn-2" onclick="location.href='./Consumables/index.php'"><span class="btn-text">Consumables</span></button>
+            <button class="btn-3" onclick="location.href='./Inventory/index.php'"><span class="btn-text">Inventory</span></button>
+            <button class="btn-4" onclick="location.href='./Orders/index.php'"><span class="btn-text">Orders</span></button>
+        </div>
+    </div>
+</body>
+</html>
