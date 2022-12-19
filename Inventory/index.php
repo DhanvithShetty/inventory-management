e45bdbf1fc919a3c808593b02098ea8c5eb69dcc
Dhanvith Shetty <79599525+DhanvithShetty@users.noreply.github.com>
2022-12-20 02:31:37 +0530

    Passed sno instead of inv_id to the .php files

--- Inventory/index.php
+++ Inventory/index.php
@@ -6,18 +6,20 @@
     <title>Inventory Management</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
 </head>
-<body>
+<body style = "background: linear-gradient(40deg, #a1ffce, #faffd1)">
     <button class="btn btn-primary" onclick="location.href='../home.php'">Back</button>
     <div class = "container my-5">
-        <h2 class="text-center my-5">Inventory Table</h2>
+        <h2 class="text-center my-5"><u>Inventory Table</u></h2>
         <a class="btn btn-primary mb-5" href="create.php" role="button">Add Item</a>
         <br>
         <table class="table">
             <thead>
                 <tr>
+                    <th>Serial No.</th>
                     <th>Inventory ID</th>
                     <th>Item ID</th>
-                    <th>Quantity</th>
+                    <th>Item Type</th>
+                    <th>Item Name</th>
                     <th>Condition</th>
                     <th>Action</th>
                 </tr>
@@ -35,7 +37,7 @@
                         die("Connection failed: ". $connection->connect_error);
                     }
 
-                    $sql = "SELECT * FROM `inventory`";
+                    $sql = "SELECT * FROM `inventory`,`item` where inventory.item_id = item.item_id";
                     $result = $connection->query($sql);
 
                     if(!$result) {
@@ -45,13 +47,15 @@
                 while ($row = $result->fetch_assoc()) {
                     echo "
                         <tr>
+                            <td>$row[sno]</td>
                             <td>$row[inv_id]</td>
                             <td>$row[item_id]</td>
-                            <td>$row[quantity]</td>
+                            <td>$row[item_type]</td>
+                            <td>$row[item_name]</td>
                             <td>$row[condition]</td>
                             <td>
-                                <a class='btn btn-primary btn-sm' href='/dbms/Inventory/edit.php?id=$row[inv_id]'>Edit</a>
-                                <a class='btn btn-danger btn-sm' href='/dbms/Inventory/delete.php?id=$row[inv_id]'>Delete</a>
+                                <a class='btn btn-primary btn-sm' href='/dbms/Inventory/edit.php?id=$row[sno]'>Edit</a>
+                                <a class='btn btn-danger btn-sm' href='/dbms/Inventory/delete.php?id=$row[sno]'>Delete</a>
                             </td>
                         </tr>
                         ";
